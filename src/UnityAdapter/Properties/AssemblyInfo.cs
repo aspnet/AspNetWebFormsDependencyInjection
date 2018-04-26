9842688a869837dd0fa7371bd807876f9fcfe0d5
Matt Fei <mattfei@microsoft.com>
2018-04-26 13:41:49 -0700

    prototype

---
+++ src/UnityAdapter/Properties/AssemblyInfo.cs
@@ -0,0 +1,24 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Microsoft.AspNet.WebFormsDependencyInjection.Unity")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft Corporation")]
+[assembly: AssemblyProduct("Microsoft.AspNet.WebFormsDependencyInjection.Unity")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("3e53288e-f0d1-4a06-8ff9-7dc8fadfdc62")]
+
---
+++ src/UnityAdapter/UnityContainerAdapter.cs
@@ -0,0 +1,95 @@
+﻿
+
+namespace Microsoft.AspNet.WebFormsDependencyInjection.Unity
+{
+    using global::Unity;
+    using global::Unity.Exceptions;
+    using Microsoft.AspNet.WebFormsDependencyInjection.Unity.Resources;
+    using System;
+    using System.Collections.Concurrent;
+    using System.Reflection;
+    using System.Web;
+
+    /// <summary>
+    /// The Unity adapter for WebObjectActivator
+    /// </summary>
+    public class UnityContainerAdapter : IServiceProvider
+    {
+        private static UnityContainerAdapter _adapter;
+        private static object _lock = new object();
+
+        private IUnityContainer _container;
+        private ConcurrentDictionary<Type, bool> _typesCannotResolve = new ConcurrentDictionary<Type, bool>();
+
+        internal Func<Type, object> CreateNonPublicInstance =
+                    (serviceType) => Activator.CreateInstance(
+                        serviceType,
+                        BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.CreateInstance,
+                        null,
+                        null,
+                        null);
+
+        internal UnityContainerAdapter(IUnityContainer container)
+        {
+            _container = container;
+        }
+
+        /// <summary>
+        /// Implementation of IServiceProvider. Asp.net will call this method to
+        /// create the instances of Page/UserControl/HttpModule etc.
+        /// </summary>
+        /// <param name="serviceType"></param>
+        /// <returns></returns>
+        public object GetService(Type serviceType)
+        {
+            if (_typesCannotResolve.ContainsKey(serviceType))
+            {
+                return CreateNonPublicInstance(serviceType);
+            }
+
+            try
+            {
+                return _container.Resolve(serviceType);
+            }
+            catch (ResolutionFailedException)
+            {
+                _typesCannotResolve.TryAdd(serviceType, true);
+                return CreateNonPublicInstance(serviceType);
+            }
+        }
+
+        /// <summary>
+        /// Plugin the adapter to WebObjectActivator and register the types in Unity container
+        /// </summary>
+        /// <param name="registerTypes"></param>
+        /// <returns></returns>
+        public static IUnityContainer RegisterWebObjectActivator(Action<IUnityContainer> registerTypes)
+        {
+            if (registerTypes == null)
+            {
+                throw new ArgumentNullException(nameof(registerTypes));
+            }
+            if(_adapter != null)
+            {
+                throw new InvalidOperationException(SR.Cannot_Register_WebObjectActivator_More_Than_Once);
+            }
+
+            lock (_lock)
+            {
+                if (_adapter == null)
+                {
+                    var container = new UnityContainer();
+                    _adapter = new UnityContainerAdapter(container);
+                    HttpRuntime.WebObjectActivator = _adapter;
+                    registerTypes(container);
+
+                    return container;
+                }
+                else
+                {
+                    throw new InvalidOperationException(SR.Cannot_Register_WebObjectActivator_More_Than_Once);
+                }
+            }
+        }
+    }
+}
